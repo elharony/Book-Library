98b8dfc8c1b7a6f8e1ff10b7a012d15a286161aa
elharony <p.yahyaelharony@gmail.com>
2018-07-14 02:02:08 +0200

    Use '<Link>' component instead of '<a>' tag to back to '/' path

--- src/SearchBook.js
+++ src/SearchBook.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import * as BooksAPI from './BooksAPI'
+import { Link } from 'react-router-dom'
 import escapeStringRegExp from 'escape-string-regexp'
 import sortBy from 'sort-by'
 
@@ -33,7 +34,7 @@ class SearchBook extends Component {
         return (
             <div className="search-books">
                 <div className="search-books-bar">
-                    <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+                    <Link to='/' className="close-search">Close</Link>
                     <div className="search-books-input-wrapper">
                         <input
                             type="text"
