00df8c6d22d14b304c2a9a34f2102c0a4c778b89
elharony <p.yahyaelharony@gmail.com>
2018-07-18 14:40:15 +0200

    bug: App crashes if a book doesn't have a thumbnail

--- src/Book.js
+++ src/Book.js
@@ -21,27 +21,73 @@ class Book extends Component {
 
         const {thisBook} = this.props
 
-        return(
-            <li>
-                <div className="book">
-                    <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${thisBook.imageLinks.thumbnail})` }}></div>
-                        <div className="book-shelf-changer">
-                            <select onChange={(e) => {this.changeShelf(thisBook, e.target.value)}}
-                                    value={thisBook.shelf}>
-                                <option value="move" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                            </select>
+        if(thisBook.imageLinks) {
+            return(
+                <li>
+                    <div className="book">
+                        <div className="book-top">
+                            <div 
+                                className="book-cover" 
+                                style={{ 
+                                    width: 128, 
+                                    height: 193, 
+                                    backgroundImage: `url(${thisBook.imageLinks.thumbnail})`,
+                                    backgroundSize: 'cover',
+                                    backgroundPosition: 'center'
+                                }}
+                            >
+                                    
+                            </div>
+                            <div className="book-shelf-changer">
+                                <select onChange={(e) => {this.changeShelf(thisBook, e.target.value)}}
+                                        value={thisBook.shelf}>
+                                    <option value="move" disabled>Move to...</option>
+                                    <option value="currentlyReading">Currently Reading</option>
+                                    <option value="wantToRead">Want to Read</option>
+                                    <option value="read">Read</option>
+                                    <option value="none">None</option>
+                                </select>
+                            </div>
                         </div>
+                        <div className="book-title">{thisBook.title}</div>
+                        <div className="book-authors">{thisBook.authors ? thisBook.authors.toString() : 'Nothing!'}</div>
                     </div>
-                    <div className="book-title">{thisBook.title}</div>
-                    <div className="book-authors">{thisBook.authors ? thisBook.authors.toString() : 'Nothing!'}</div>
-                </div>
-            </li>
-        )
+                </li>
+            )
+        } else {
+            return(
+                <li>
+                    <div className="book">
+                        <div className="book-top">
+                            <div 
+                                className="book-cover" 
+                                style={{ 
+                                    width: 128, 
+                                    height: 193, 
+                                    backgroundImage: `url(https://ibf.org/site_assets/img/placeholder-book-cover-default.png)`,
+                                    backgroundSize: 'cover',
+                                    backgroundPosition: 'center'
+                                }}
+                            >
+                                    
+                            </div>
+                            <div className="book-shelf-changer">
+                                <select onChange={(e) => {this.changeShelf(thisBook, e.target.value)}}
+                                        value={thisBook.shelf}>
+                                    <option value="move" disabled>Move to...</option>
+                                    <option value="currentlyReading">Currently Reading</option>
+                                    <option value="wantToRead">Want to Read</option>
+                                    <option value="read">Read</option>
+                                    <option value="none">None</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div className="book-title">{thisBook.title}</div>
+                        <div className="book-authors">{thisBook.authors ? thisBook.authors.toString() : 'Nothing!'}</div>
+                    </div>
+                </li>
+            )
+        }
     }
 }
 
